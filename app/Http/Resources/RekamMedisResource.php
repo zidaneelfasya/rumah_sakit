ce50430cdc421fe102a7227e2fdd4c82813caccc
zidaneelfasya <111755264+zidaneelfasya@users.noreply.github.com>
2024-09-01 22:11:44 +0700

    Create RekamMedisResource.php

---
+++ app/Http/Resources/RekamMedisResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class RekamMedisResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'tanggal_kunjungan' => $this->tanggal_kunjungan,
+            'diagnosa' => $this->dx,
+            'tindakan' => $this->tx,
+            'keterangan' => $this->keterangan,
+            'pasien' => $this->datapasien,
+        ];
+    }
+}
