e80ea639dd50352bcc0c2d85d4dcdb58a65bc8f3
zidaneelfasya <111755264+zidaneelfasya@users.noreply.github.com>
2024-09-01 22:12:13 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,19 @@
 <?php
 
+use App\Models\Rekam_medis;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PasienController;
+use App\Http\Controllers\RekamMedisController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/home', function () {
+Route::get('/admin', function () {
     return view('home');
 });
+Route::get('/admin/home', function () {
+    return view('home');
+});
+Route::get('admin/pasien', [PasienController::class, 'lihatPasien']);
+Route::get('admin/detail', [PasienController::class, 'lihatPasien']);
+Route::get('admin/rekam', [RekamMedisController::class, 'lihatRekam_medis']);
