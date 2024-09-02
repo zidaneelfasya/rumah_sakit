1d4976c8a62a5c3f7f323f6ef8c929bedd83bbc9
zidaneelfasya <elfasyazidan1@gmail.com>
2024-09-02 19:09:55 +0700

    Update formaddpasien.blade.php

--- resources/views/formaddpasien.blade.php
+++ resources/views/formaddpasien.blade.php
@@ -19,24 +19,16 @@
                                 
                             </div>
                             <div class="mb-3">
-                                <label for="nim" class="form-label">NIM</label>
+                                <label for="nim" class="form-label">NIK</label>
                                 <input type="number" class="form-control" id="nim" name="nim">
                             </div>
                             <div class="mb-3">
-                                <label for="email" class="form-label">Email</label>
-                                <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp">
-                              </div>
-                            <div class="mb-3">
-                                <label for="password" class="form-label">Password</label>
-                                <input type="password" class="form-control" id="password" name="password">
+                                <label for="alamat" class="form-label">Alamat</label>
+                                <input type="text" class="form-control" id="alamat" name="alamat">
                             </div>
-                            <div>
-                                <label for="role" class="form-label">Role</label>
-                                <select class="form-control" aria-label=".form-select-lg example" name="role">
-                                <option selected>Pilih Role</option>
-                                <option value="1">Admin</option>
-                                <option value="2">Staff Muda</option>
-                            </select>
+                            <div class="mb-3">
+                                <label for="tlahir" class="form-label">Tanggal Lahir</label>
+                                <input type="date" class="form-control" id="tlahir" name="tlahir">
                             </div>
                             <div>
                                 <label class="col-sm-2 col-form-label"></label>
