ac4fdb6c52119aef31d37b2021a655fb1640f703
zidaneelfasya <elfasyazidan1@gmail.com>
2024-09-02 20:50:54 +0700

    Create formupdatepasien.blade.php

---
+++ resources/views/pasien/formupdatepasien.blade.php
@@ -0,0 +1,52 @@
+@extends('layout/main')
+
+@section('breadcrumbs')
+
+@endsection
+
+@section('content')
+<div class="container-fluid">
+    <div class="container-fluid">
+        <div class="card mx-auto"  style="width: 500px" >
+            <div class="card-body">
+                <div class="row">
+                    
+                        <form id="formUpdatePost" action="/admin/pasien/update/{{$pasien->id}}/store" method="post" enctype="multipart/form-data" onsubmit="submitForm()">
+                            @csrf
+                            <div class="mb-3">
+                                <label for="nama" class="form-label">Nama</label>
+                                <input type="text" class="form-control" id="nama" name="nama" value="{{$pasien->nama}}">
+                                
+                            </div>
+                            <div class="mb-3">
+                                <label for="NIK" class="form-label">NIK</label>
+                                <input type="number" class="form-control" id="NIK" name="NIK" value="{{$pasien->NIK}}">
+                            </div>
+                            <div class="mb-3">
+                                <label for="alamat" class="form-label">Alamat</label>
+                                <input type="text" class="form-control" id="alamat" name="alamat" value="{{$pasien->alamat}}">
+                            </div>
+                            <div class="mb-3">
+                                <label for="tlahir" class="form-label">Tanggal Lahir</label>
+                                <input type="date" class="form-control" id="tlahir" name="tlahir" value="{{$pasien->tanggal_lahir}}">
+                            </div>
+                            <div>
+                                <label class="col-sm-2 col-form-label"></label>
+                                <div class="col-sm-10">
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                </div>
+                            </div>
+                            <br>
+                        </form>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+    
+    
+<script>
+</script>
+@endsection
