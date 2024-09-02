81175585fd734a555f2f52c469f9a3f0bd819aad
zidaneelfasya <elfasyazidan1@gmail.com>
2024-09-02 19:49:42 +0700

    Update PasienController.php

--- app/Http/Controllers/PasienController.php
+++ app/Http/Controllers/PasienController.php
@@ -17,4 +17,21 @@ class PasienController extends Controller
         $pasien= Pasien::all(); 
         return response()->json($pasien);
     }
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nama' => 'required|string|max:255',
+            'NIK' => 'required|numeric|unique:pasiens,NIK', 
+            'alamat' => 'required|string|max:255',
+            'tlahir' => 'required|date',
+        ]);
+        Pasien::create([
+            'nama' => $request->nama,
+            'NIK' => $request->NIK,
+            'alamat' => $request->alamat,
+            'tanggal_lahir' => $request->tlahir,
+        ]);
+        return redirect('/admin/pasien')->with('success', 'Data pasien berhasil ditambahkan!');
+    }
+
 }
