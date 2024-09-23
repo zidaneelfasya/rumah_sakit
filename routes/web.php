768534000913731ef571de38789dcc623d70c00c
zidaneelfasya <elfasyazidan1@gmail.com>
2024-09-23 22:37:03 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\Rekam_medis;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiController;
 use App\Http\Controllers\PasienController;
 use App\Http\Controllers\RekamMedisController;
 
@@ -18,21 +19,25 @@ Route::get('/appointment', function () {
 Route::get('/admin', function () {
     return view('home');
 });
+Route::get('/admin/pasien/add', function () {
+    return view('pasien/formaddpasien');
+});
 Route::get('/admin/home', function () {
     return view('home');
 });
 Route::get('admin/pasien', [PasienController::class, 'lihatPasien']);
 Route::get('admin/detail', [PasienController::class, 'lihatPasien']);
 Route::get('admin/pasien/{id}', [PasienController::class, 'detailPasien']);
-Route::get('/admin/pasien/add', function () {
-    return view('pasien/formaddpasien');
-});
+
 Route::post('admin/pasien/add/store', [PasienController::class, 'store']);
 Route::get('admin/pasien/update/{id}', [PasienController::class, 'pasienParam']);
 Route::post('admin/pasien/update/{id}/store', [PasienController::class, 'update']);
 Route::get('admin/pasien/delete/{id}', [PasienController::class, 'delete']);
 
 
+Route::get('/admin/rekam/add', function () {
+    return view('rekamMedis/formadd');
+});
 Route::get('admin/rekam', [RekamMedisController::class, 'lihatRekam_medis']);
 Route::get('admin/rekam/{id}', [RekamMedisController::class, 'detailRekamMedis'])->name('rekam.detail');
 
@@ -44,3 +49,6 @@ Route::get('admin/rekam/delete/{id}', [RekamMedisController::class, 'delete']);
 Route::get('/dokter/rekam', function () {
     return view('dokter/rekammedis');
 });
+
+Route::get('api/pasien', [ApiController::class, 'lihatPasien']); 
+Route::get('api/pasien/{id}', [ApiController::class, 'detailPasien']);
