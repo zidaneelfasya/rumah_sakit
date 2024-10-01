6c505970fd12a78863e4a63161403e9167557374
zidaneelfasya <elfasyazidan1@gmail.com>
2024-10-01 13:04:25 +0700

    Update ApiController.php

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Pasien;
+use App\Models\RekamMedis;
 use Illuminate\Http\Request;
 
 class ApiController extends Controller
@@ -12,9 +13,19 @@ class ApiController extends Controller
         $pasien= Pasien::all(); 
         return response()->json($pasien);
     }
+    public function lihatPasienDetail()
+    {
+        $pasien = Pasien::with('rekamMedis')->get();
+        return response()->json($pasien);
+    }
     public function detailPasien($id)
     {
         $pasien = Pasien::with('rekamMedis')->findOrFail($id);
         return response()->json($pasien);
+    }
+    public function lihatRekam()
+    {
+        $rm = RekamMedis::all();
+        return response()->json($rm);
     }
 }
