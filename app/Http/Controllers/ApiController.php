b25a500b73549e0dcbf1d1bcaf92c19fc66c7081
Zidane Elfasya <111755264+zidaneelfasya@users.noreply.github.com>
2024-10-03 16:36:04 +0700

    Update ApiController.php

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Http\Response;
 use App\Models\Pasien;
 use App\Models\RekamMedis;
 use Illuminate\Http\Request;
@@ -27,5 +27,45 @@ class ApiController extends Controller
     {
         $rm = RekamMedis::all();
         return response()->json($rm);
+    }
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nama' => 'required|string|max:255',
+            'NIK' => 'required|numeric|unique:pasiens,NIK', 
+            'alamat' => 'required|string|max:255',
+            'tlahir' => 'required|date',
+        ]);
+        $pasien = Pasien::create([
+            'nama' => $request->nama,
+            'NIK' => $request->NIK,
+            'alamat' => $request->alamat,
+            'tanggal_lahir' => $request->tlahir,
+        ]);
+        
+        return response()->json($pasien);
+    }
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'nama' => 'required|string|max:255',
+            'NIK' => 'required|numeric|unique:pasiens,NIK,' . $id, 
+            'alamat' => 'required|string|max:255',
+            'tlahir' => 'required|date',
+        ]);
+        $pasien = Pasien::findOrFail($id);
+        $pasien->update([
+            'nama' => $request->nama,
+            'NIK' => $request->NIK,
+            'alamat' => $request->alamat,
+            'tanggal_lahir' => $request->tlahir,
+        ]);
+        return response()->json($pasien);
+    }
+    public function delete($id)
+    {
+        $pasien = Pasien::findOrFail($id);
+        $pasien->delete();
+        return response()->json('berhasil dihapus');
     }
 }
